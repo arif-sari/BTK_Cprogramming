2372d7850da4e328a40d4edacf2734c7e8292fc7
Arif SARI <72504353+arif-sari@users.noreply.github.com>
2024-08-23 16:21:13 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int i,n;
+
+    //kullanıcıdan veri alma
+
+    printf("Lütfen üst sınır değerini giriniz : ");
+    scanf("%d",&n);
+
+    printf("1 den %d ye kadar sayma sayılarıı : \n", &n);
+
+    for(i=1; i<=n; i++)
+    {
+        printf("%d \n",i);
+
+    }
+
+    return 0;
+}
