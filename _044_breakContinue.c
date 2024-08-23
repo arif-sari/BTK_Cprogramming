51a98527a3e6c639ac5db8696088a2f8563c91af
Arif SARI <72504353+arif-sari@users.noreply.github.com>
2024-08-23 17:13:13 +0300

    Create _044_break.c

---
+++ _044_breakContinue.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int i=0;
+
+    for(i=1; i<=10;i++)
+        {
+    if(i==4){
+        break;
+    }
+        printf("%d\n",i);
+
+    }
+return 0;
+
+
+}
