0c5ec98287b0ac3a362b25092a38e3e18841ebc7
Arif SARI <72504353+arif-sari@users.noreply.github.com>
2024-08-26 21:45:42 +0300

    Update _049_enbuyukDeger.c

--- _049_enbuyukDeger.c
+++ _049_enbuyukDeger.c
@@ -1,1 +1,22 @@
 
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int benimDizim[] ={3,8,11,7,2,9,5,4};
+    int enBuyukDeger = benimDizim[0];
+    int konum = 0;
+
+    int boyut= sizeof(benimDizim)/sizeof(benimDizim[0]);
+
+    for(int indeks=0;indeks<boyut;indeks++){
+        if(benimDizim[indeks]>enBuyukDeger){
+            enBuyukDeger = benimDizim[indeks];
+            konum= indeks;
+        }
+    }
+
+    printf("Dizinin en buyuk elemani, %d indeksli konumda bulunan %d degeridir \n",konum,enBuyukDeger);
+    return 0;
+}
