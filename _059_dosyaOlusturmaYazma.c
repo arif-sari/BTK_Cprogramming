d5a9ed0d9cf16d1976adcd35a795b74120c4cd46
Arif SARI <72504353+arif-sari@users.noreply.github.com>
2024-09-05 17:05:55 +0300

    Create _059_dosyaOlusturmaYazma.c

---
+++ _059_dosyaOlusturmaYazma.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define VERI_BOYUTU 1000
+
+
+int main()
+{
+    /*veri boyutu depolayan karakter dizizi */
+    char [VERI_BOYUTU];
+
+  /*File Pointer to hold referance to our file*/
+  FILE * fPtr;
+
+  // w (write) modunda aç
+  // "data/file.txt" dosya güzergahı
+
+  fPtr = fopen("dosya1.txt","w+");
+
+  //fopen() başarısız olursa null dönecek
+
+  if(fPtr==NULL){
+
+    printf("Dosya olusturulamadı. \n");
+    exit(EXIT_FAILURE);
+    }
+
+    printf("Dosyaya kaydedilecek olan girdiyi al: \n");
+    fgets(veri,VERI_BOYUTU,stdin);
+
+    //veriyi dosyaya yaz
+
+    fputs(ver, fPtr);
+
+    // Dosyayı kapat
+
+    fclose(fPtr);
+
+    // başarı ile mesaj yazdırıldı
+
+    print("Dosya başarı ile yazdırıldı ve içeriği kaydedildi... ");
+
+
+    return 0;
+}
