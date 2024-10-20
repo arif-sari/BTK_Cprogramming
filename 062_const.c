5733e5dbda25974fffd02f13a2ed0b4f13c83650
Arif SARI <72504353+arif-sari@users.noreply.github.com>
2024-10-21 00:18:35 +0300

    Create 062_const.c

---
+++ 062_const.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+   const int SaattekiDakikaSayisi =60;
+   const float PI = 3.14;
+
+   printf("%d\n", SaattekiDakikaSayisi);
+   printf("%d\n", PI);
+
+   return 0;
